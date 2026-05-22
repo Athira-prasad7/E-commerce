6e869afe32c9d6fa62502f23cc24e72864ee2822
athira <at100mix@gmail.com>
2026-05-23 00:20:39 +0530

    detailproduct related products

--- frontend/src/Pages/ProductDetails.jsx
+++ frontend/src/Pages/ProductDetails.jsx
@@ -45,9 +45,10 @@ function ProductDetails() {
         }
         addToCart(product.id);
     }
+    // className="bg-blue-600 text-white px-4 py-2  rounded-lg hover:bg-blue-700 transition"
         return (
-        <div className="min-h-screen bg-gray-100 flex py-6 pl-50 item-center justify-center">
-            <div className="p-7 max-w-3xl w-full">
+        <div className="min-h-screen bg-gray-100 flex py-40 pl-50">
+            <div className="max-w-3xl w-full">
                 <div className="flex flex-col md:flex-row gap-8">
                     <img
                         src={product.image}
@@ -55,14 +56,14 @@ function ProductDetails() {
                         className="w-full md:w-1/2 h-auto object-cover rounded-lg"
                     />
                     <div className="flex-1">
-                        <h1 className="text-3xl font-bold text-gray-800 mb-2">{product.name}</h1>
-                        <p className="text-xl text-gray-600 font-semibold mg-4">${product.price}</p>
-                        <p className="text-gray-700 leading-relaxed">{product.description}</p>
-                        <button onClick={handleAddToCart} className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition">
+                        <h1 className="text-3xl font-bold text-gray-800 p-5">{product.name}</h1>
+                        <p className="text-xl text-gray-600 font-semibold p-5">${product.price}</p>
+                        <p className="text-gray-700 leading-relaxed p-5">{product.description}</p>
+                        <button onClick={handleAddToCart} className="border bg-blue-400 text-gray-800 px-5 py-2 ml-5 w-fit hover:bg-gray-100 transition">
                             Add to Cart 🛒
                         </button>
                         <div className="mt-4">
-                            <a href="/" className="text-blue-600 hover:underline">
+                            <a href="/" className="text-blue-600 hover:underline px-5">
                                 &larr; Back to Home
                             </a>
                         </div>
